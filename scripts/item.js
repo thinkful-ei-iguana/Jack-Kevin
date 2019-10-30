c86f7a1ad69a17a3b2f3890fd82c819dfd3779b5
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-30 11:56:41 -0400

    added item functions

--- scripts/item.js
+++ scripts/item.js
@@ -1,4 +1,19 @@
+function validateName(name) {
+  if (name === '') {
+    throw new TypeError('name must not be blank');
+  }
+}
+  
+function create(name) {
+  let newItem = {
+    id:cuid(),
+    name:name,
+    checked:false
+  };
+  return newItem;
+}
 
-
-
-export default {};
+export default {
+  validateName,
+  create
+};
