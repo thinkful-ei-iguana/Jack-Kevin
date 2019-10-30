ff4c9769f59c71e0feb0273498a27fbe2b4a7a15
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-30 12:27:28 -0400

    added functions to store

--- scripts/store.js
+++ scripts/store.js
@@ -3,7 +3,42 @@ import item from './item.js';
 let items = [];
 let hideCheckedItems = false;
 
+function findById(id) {
+  console.log(items.find(item => item.id === id));
+  return items.find(item => item.id === id);
+}
+
+function addItem(name) {
+    try{
+        item.validateName(name);
+        let newItem = item.create(name);
+        this.items.push(newItem);
+    }catch { console.log(`cannot add item ${error.message}`);}
+}
+
+function findAndToggleChecked(id) {
+    this.findById(id).checked = !this.findById(id).checked;
+}
+
+function findAndUpdateName(id, newName) {
+    try {
+        item.validateName(newName);
+        item.findById(id).name = newName;
+    }catch { console.log(`Cannot update name: ${error.message}`);
+    }
+}
+
+function findAndDelete(id) {
+   let deleteItem = this.findById(id);
+   this.items = this.items.filter(item => item != deleteItem);
+}
+
 export default {
-    items,
-    hideCheckedItems
+  items,
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete,
 };
